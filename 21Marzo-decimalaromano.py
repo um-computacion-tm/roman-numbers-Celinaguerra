4eb221c0e0a0411683a373ba6cd1107a881a44a0
Celinaguerra <celinaguerradiaz@gmail.com>
2023-03-26 21:11:08 -0300

    1-39

--- 21Marzo-decimalaromano.py
+++ 21Marzo-decimalaromano.py
@@ -1,19 +1,56 @@
 import unittest
-#ver si se modifica
 def decimal_to_roman(decimal):
+
+################ VARIABLES ##################
+
+    totaldecimal = ""
+
+    if decimal == 50:
+        return "L"
+    elif decimal == 100:
+        return "C"
+    elif decimal == 500:
+        return "D"
+    elif decimal == 1000:
+       return "M"
+
+
+############### 1-39 ####################
+
+    if decimal > 10 and decimal < 40:
+        totaldecimal = "X" * (decimal //10)
+        decimal = decimal % 10
     if decimal <=3:
-        return "I"*decimal
-    elif decimal >5 and decimal <9:
-        return "V" + "I"*(decimal-5)
+        return totaldecimal + "I"*decimal    
     elif decimal == 5:
-        return "V"
+        return totaldecimal + "V"
+    elif decimal >5 and decimal <9:                                 #recordatorio para cambiar nombres de variables
+        return totaldecimal + ("V" + "I"*(decimal-5))                           #para evitar confusiones
     elif decimal ==10:
-        return "X"
-    elif decimal == 4:
-        return "IV"
-    elif decimal == 9:
-        return "IX"
+        return totaldecimal + "X"
+    if decimal == 4:
+        return totaldecimal + "IV"
+    if decimal == 9:
+        return totaldecimal + "IX"
     
+############ algunas centenas #############
+
+    num = ""
+    if decimal >= 100:
+        centena = decimal // 100
+        num = 'C' * centena
+        decimal = decimal % 100 
+    if decimal <= 3:
+        num += 'I' * decimal
+    elif decimal == 5:
+        num += 'V'
+    elif decimal == 10:
+        num += "X"
+    elif decimal >5 and decimal <9:
+        num += "V" + "I"*(decimal-5)  
+    return num
+
+
 
 class TestDecimalToRoman(unittest.TestCase):
     def test_uno(self):
@@ -61,6 +98,22 @@ class TestDecimalToRoman(unittest.TestCase):
     def test_nueve(self):
         resultado = decimal_to_roman(9)
         self.assertEqual(resultado,"IX")
+    
+    def test_24(self):
+        resultado = decimal_to_roman(24)
+        self.assertEqual(resultado,"XXIV")
+    
+    def test_37(self):
+        resultado = decimal_to_roman(37)
+        self.assertEqual(resultado,"XXXVII")
+
+    def test_105(self):
+        resultado = decimal_to_roman(105)
+        self.assertEqual(resultado,"CV")
+
+    def test_207(self):
+        resultado = decimal_to_roman(207)
+        self.assertEqual(resultado,"CCVII")
 
 if __name__=="__main__":
     unittest.main()
