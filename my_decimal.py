48d9c9770f96d0a754d68bf8fc2d9960f56a260d
Celinaguerra <celinaguerradiaz@gmail.com>
2023-04-09 19:12:55 -0300

    roman2decimal--TERMINADO

--- my_decimal.py
+++ my_decimal.py
@@ -4,7 +4,7 @@ def roman_to_decimal(roman):
     listadecimales = [1, 5, 10, 50, 100, 500, 1000]
     decimal = 0
     anterior = 0
-
+#
     for num in roman[::-1]:
         indice=listaromanos.index(num)
         if listadecimales[indice] >= anterior:
